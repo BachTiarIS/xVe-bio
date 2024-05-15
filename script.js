6e6b79f62137991017e5d1f43a6e11d127f9515c
BachTiarIs <njirbhu@gmail.com>
2024-05-15 22:42:37 +0700

    *add theme switch *add some style

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ function tambah() {
  }
 
 function kurang() {
-    var num1 = parseFloat(document.getElementById('num1').value);
-    var num2 = parseFloat(document.getElementById('num2').value);
-    var output = num1 - num2;
-    document.getElementById('output').value = output;
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    document.getElementById('output').value = num1 - num2;
 }
 
 function kali() {
@@ -24,6 +23,18 @@ function bagi() {
     var num2 = parseFloat(document.getElementById('num2').value);
     var output = num1 / num2;
     document.getElementById('output').value = output;
-}
+ }
+
+
+
+// Ambil tombol atau elemen yang akan digunakan untuk mengganti tema
+const themeButton = document.getElementById('toggleTheme');
+const body = document.body;
+
+// Tambahkan event listener untuk mengubah tema
+toggleTheme.addEventListener('click', () => {
+    // Ganti kelas CSS pada elemen body untuk mengubah tema
+    body.classList.toggle('dark');
+});
     
 
